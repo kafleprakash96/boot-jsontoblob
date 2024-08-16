b151523da1810972fbafda2b030f7f23e84ed4db
Prakash Kafle <kafleprakash96@gmail.com>
2024-08-15 19:20:04 -0500

    Added GET endpoint to get blob data as json

--- src/main/java/com/example/jsontoblob/controller/ArticleController.java
+++ src/main/java/com/example/jsontoblob/controller/ArticleController.java
@@ -1,14 +1,13 @@
 package com.example.jsontoblob.controller;
 
 import com.example.jsontoblob.dto.ArticleDto;
+import com.example.jsontoblob.model.Article;
 import com.example.jsontoblob.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 @RestController
@@ -21,5 +20,10 @@ public class ArticleController {
     @PostMapping("/save")
     public ResponseEntity<ArticleDto> saveArticle(@RequestBody ArticleDto articleDto) throws SQLException {
         return ResponseEntity.ok().body(articleService.saveArticle(articleDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDto> getArticleById(@PathVariable Long id) throws SQLException, IOException {
+        return ResponseEntity.ok().body(articleService.getArticleById(id));
     }
 }
--- src/main/java/com/example/jsontoblob/service/ArticleService.java
+++ src/main/java/com/example/jsontoblob/service/ArticleService.java
@@ -3,11 +3,13 @@ package com.example.jsontoblob.service;
 import com.example.jsontoblob.dto.ArticleDto;
 import com.example.jsontoblob.model.Article;
 import com.example.jsontoblob.repo.ArticleRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.sql.Blob;
 import java.sql.SQLException;
@@ -21,6 +23,7 @@ public class ArticleService {
     @Autowired
     private ModelMapper modelMapper;
 
+
     public ArticleDto saveArticle(ArticleDto articleDto) throws SQLException {
         Article article = modelMapper.map(articleDto,Article.class);
 
@@ -30,5 +33,25 @@ public class ArticleService {
         article.setContent(blobOfContent);
         articleRepository.save(article);
         return modelMapper.map(article,ArticleDto.class);
+    }
+
+    public ArticleDto getArticleById(Long id) throws SQLException, IOException {
+        Article article = articleRepository.findById(id).orElseThrow(()-> new RuntimeException("Article not found"));
+
+        //Get the blob content
+        Blob blob = article.getContent();
+
+        //Convert blob to String
+        String releaseNoteString = convertBlobToString(blob);
+
+        ArticleDto articleDto = modelMapper.map(article,ArticleDto.class);
+
+        articleDto.setContent(releaseNoteString);
+
+        return articleDto;
+    }
+
+    private String convertBlobToString(Blob blob) throws SQLException {
+        return new String(blob.getBytes(1,(int)blob.length()));
     }
 }
